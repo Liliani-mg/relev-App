0a6f0da2b4081ac0c913fd8a3a9aaefa5aa0991e
Irupe <lilianirupemeza@gmail.com>
2022-12-20 14:56:44 -0300

    <fix>reorder routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,9 +3,9 @@ const { get, create, editUser, getOne } = require("../controllers/users");
 
 const router = express.Router();
 
+router.post("/", create);
 router.get("/", get);
 router.get("/:id", getOne)
-router.post("/", create);
 router.put("/:id", editUser)
 
 module.exports = router;
