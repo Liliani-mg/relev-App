b93ec1f9fb6718301167a8b105b81ef9ae480e9e
Irupe <lilianirupemeza@gmail.com>
2023-02-07 10:45:19 -0300

    delete seed user

--- api/database/seeders/20221220115123-seed-user.js
+++ api/database/seeders/20221220115123-seed-user.js
@@ -2,30 +2,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-      {
-      name: 'Jose Balcarce',
-      roleId: 2,
-      email: 'joseb@mail.com',
-      password: 'Hola1234',
-      areaId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
+     {
       name: 'User Admin',
       roleId: 1,
       email: 'admin@mail.com',
       password: 'Hola1234',
       areaId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'Juan Gomez',
-      roleId:3,
-      email: 'juang@mail.com',
-      password: 'Hola1234',
-      areaId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     }
